ea14255024f30fff6bab43a6bbc547564a67810d
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2020-05-04 23:57:25 +0900

    毎分Testを送り続けるクソ仕様

--- serverless/src/schedule.ts
+++ serverless/src/schedule.ts
@@ -1,14 +1,36 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
 
+import { URLSearchParams } from 'url';
+import axios from 'axios';
+import { setupFireStore } from './common/firestore';
+
+const LINE_NOTIFY_BASE_URL = 'https://notify-api.line.me';
+
 export const handler: APIGatewayProxyHandler = async (event, _context) => {
   console.log(event);
+  const messages = new URLSearchParams();
+  messages.append('message', 'testtest');
+
+  const firestore = setupFireStore();
+  const docsQuery = await firestore.collection('LineNotifyUsers').get();
+  const responses = await Promise.all(
+    docsQuery.docs.map((doc) => {
+      const docData = doc.data();
+      return axios.post(LINE_NOTIFY_BASE_URL + '/api/notify', messages, {
+        headers: {
+          Authorization: 'Bearer ' + docData.access_token,
+        },
+      });
+    }),
+  );
+  const responseBody = JSON.stringify(
+    responses.map((response) => response.data)
+  , null, 2);
+  console.log(responseBody);
 
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
+    body: responseBody,
   };
 }
