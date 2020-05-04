88d07bf905d95227ca49fcab1f0e3572f0976789
Tatsukiz <tatsukix0206@gmail.com>
2020-05-04 18:28:37 +0900

    create header and footer components

---
+++ web/components/layout/footer.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-footer color="secondary" padless>
+    <v-row no-gutters class="footer">
+      <v-col class="darken-4 py-4 text-center white--text" cols="12">
+        <div class="footer-inner">
+          <p class="footer-copyright">
+            <strong>&copy;2020</strong>
+          </p>
+        </div>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class Footer extends Vue {}
+</script>
+
+<style scoped>
+p {
+  margin: 0;
+}
+.footer a {
+  text-decoration: none;
+}
+.footer p {
+  color: white;
+}
+.footer-inner {
+  width: 1140px;
+  margin: 0 auto;
+  padding: 0;
+}
+.footer-copyright {
+  font-size: 2em;
+}
+@media screen and (max-width: 959px) {
+  .footer-inner {
+    width: 100%;
+  }
+}
+</style>
---
+++ web/components/layout/header.vue
@@ -0,0 +1,35 @@
+<template>
+  <header>
+    <v-card class="header" color="#DCDCDC" flat tile>
+      <nuxt-link to="/" target="_self">
+        <v-card-title class="logo hidden-xs-only">
+          <img src="" alt="title" height="" />
+        </v-card-title>
+        <v-card-title class="logo-left hidden-sm-and-up">
+          <img src="" alt="title" height="" />
+        </v-card-title>
+      </nuxt-link>
+    </v-card>
+  </header>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class Header extends Vue {}
+</script>
+
+<style scoped>
+header {
+  background-color: #f5d1d9;
+  z-index: 999;
+  font-size: 62.5%;
+}
+.logo {
+  justify-content: center;
+}
+.logo-left {
+  justify-content: left;
+}
+</style>
