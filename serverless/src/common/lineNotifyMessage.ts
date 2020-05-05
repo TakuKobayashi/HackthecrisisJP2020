1ab31036ce12554d23ccf42f90bf311c65122b19
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2020-05-05 15:08:31 +0900

    CSV parse to JSONそしてファイルアップロード

--- serverless/src/common/lineNotifyMessage.ts
+++ serverless/src/common/lineNotifyMessage.ts
@@ -2,6 +2,7 @@ import { URLSearchParams } from 'url';
 import axios from 'axios';
 import { setupFireStore, setupFireStorage } from './firebase';
 
+const fs = require('fs');
 const LINE_NOTIFY_BASE_URL = 'https://notify-api.line.me';
 
 export async function sendMessageToAllUsers(): Promise<any[]> {
@@ -23,6 +24,22 @@ export async function sendMessageToAllUsers(): Promise<any[]> {
   return responses.map((response) => response.data);
 }
 
+export function parseCSV(): any[] {
+  const data = fs.readFileSync("hackcrisis-jp-data.csv").toString()
+  const results = [];
+  const rows = data.split("\r\n");
+  const columnNames = rows[0].split(",");
+  for(let i = 1;i < rows.length;++i){
+    const jsonColumn = {};
+    const cells = rows[i].split(",");
+    for(let j = 0;j < columnNames.length;++j){
+      jsonColumn[columnNames[j]] = cells[j];
+    }
+    results.push(jsonColumn);
+  }
+  return results;
+}
+
 export async function generateMessageBody(): Promise<string> {
   const result = await uploadToNewJSON();
   return 'testtest';
@@ -31,6 +48,6 @@ export async function generateMessageBody(): Promise<string> {
 export async function uploadToNewJSON() {
   const bucket = setupFireStorage().bucket();
   const file = bucket.file('data/convertCSV.json');
-  const result = await file.save(JSON.stringify({}));
+  const result = await file.save(JSON.stringify(parseCSV()));
   return result;
 }
