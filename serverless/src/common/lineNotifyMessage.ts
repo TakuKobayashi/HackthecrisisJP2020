a933d87ad27a669fcbb66b6da1712ae508c01181
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2020-05-05 14:36:09 +0900

    処理の場所を変更

--- serverless/src/common/lineNotifyMessage.ts
+++ serverless/src/common/lineNotifyMessage.ts
@@ -1,12 +1,12 @@
 import { URLSearchParams } from 'url';
 import axios from 'axios';
-import { setupFireStore } from './firebase';
+import { setupFireStore, setupFireStorage } from './firebase';
 
 const LINE_NOTIFY_BASE_URL = 'https://notify-api.line.me';
 
 export async function sendMessageToAllUsers(): Promise<any[]> {
   const messages = new URLSearchParams();
-  messages.append('message', generateMessageBody());
+  messages.append('message', await generateMessageBody());
 
   const firestore = setupFireStore();
   const docsQuery = await firestore.collection('LineNotifyUsers').get();
@@ -20,10 +20,17 @@ export async function sendMessageToAllUsers(): Promise<any[]> {
       });
     }),
   );
-
   return responses.map((response) => response.data);
 }
 
-export function generateMessageBody(): string {
+export async function generateMessageBody(): Promise<string> {
+  const result = await uploadToNewJSON();
   return 'testtest';
+}
+
+export async function uploadToNewJSON() {
+  const bucket = setupFireStorage().bucket();
+  const file = bucket.file('data/convertCSV.json');
+  const result = await file.save(JSON.stringify({}));
+  return result;
 }
